f851f9b25c7b2eef824e2607d046d82b1d4af39f
_Joy.Thakkolkaran_ <134796806+PunyalanEkart@users.noreply.github.com>
2023-12-01 12:08:51 +0530

    Update and rename node.js to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// DOMContentLoaded event ensures the script runs after the HTML content is loaded
+document.addEventListener('DOMContentLoaded', () => {
+  // Your code here for handling user interactions or other functionalities
+  // Example: Adding an event listener to a button click
+  const myButton = document.getElementById('myButton'); // Replace 'myButton' with your button ID
+  myButton.addEventListener('click', () => {
+    // Perform actions when the button is clicked
+    // Example: Display an alert
+    alert('Button clicked!');
+  });
+
+  // Other functions or event listeners can be added here
+});
